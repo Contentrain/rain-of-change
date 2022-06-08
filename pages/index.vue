d1081a5e3ca254f856f2a71e2ac309ec37cdb7d1
ABB65 <bayhanbayramoglu@gmail.com>
2022-06-08 12:29:10 +0300

    fix component/content path

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="max-w-2xl px-4 m-auto min-h-screen py-24">
-    <div v-if="logs" class="logs divide-y divide-gray-100">
-      <LogCard v-for="log in logs" :key="log.version" :log="log" />
+  <Suspense>
+    <div class="max-w-2xl px-4 m-auto min-h-screen py-24">
+      <div v-if="logs && logs.length>0" class="logs divide-y divide-gray-100">
+        <LogCard v-for="log in logs" :key="log.ID" :log="log" />
+      </div>
+      <div v-else class="w-full text-gray-500 text-center mt-36">
+        <h1 class="text-semibold text-xl">
+          Ooopss! There are no logs to show.
+        </h1>
+      </div>
+      <div v-if="showButton" class="w-full flex justify-center">
+        <button
+          class="text-blue-500 text-bold p-2 mt-6 flex justify-start items-center"
+          @click="limit += 5"
+        >
+          Browse all updates <i class="ri-arrow-down-s-line text-2xl ml-2" />
+        </button>
+      </div>
     </div>
-
-    <div v-else class="w-full text-gray-500 text-center mt-36">
-      <h1 class="text-semibold text-xl">
-        Ooopss! There are no logs to show.
-      </h1>
-    </div>
-    <div v-if="showButton" class="w-full flex justify-center">
-      <button class="text-blue-500 text-bold p-2 mt-6 flex justify-start items-center" @click="limit += 5">
-        Browse all updates <i class="ri-arrow-down-s-line text-2xl ml-2" />
-      </button>
-    </div>
-  </div>
+    <template #fallback>
+      Loading...
+    </template>
+  </Suspense>
 </template>
 
 <script setup>
