51638c7a0be353228f517ab3a24b786e313b1ab9
ABB65 <bayhanbayramoglu@gmail.com>
2022-06-09 14:08:24 +0300

    Cover image update

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <Html lang="en" />
     <Title>Rain of Change</Title>
     <Meta name="description" content="A free Changelog template created with Nuxt 3.0 & Nuxt Content 2.0 that you can use for your projects." />
-    <Meta property="og:image" content="static/cover.png" />
+    <Meta property="og:image" content="https://imagedelivery.net/yx26LyQGM_miwnGU8RnEaw/161e2520-980c-4c92-c953-a7080a77a800/public" />
     <Header />
     <NuxtPage />
   </div>
